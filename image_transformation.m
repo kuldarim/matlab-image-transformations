7d541ac3504f8af0d2f3763851a14389173d5e3c
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-10-11 10:04:06 -0700

    code clean up

--- image_transformation.m
+++ image_transformation.m
@@ -1,29 +1,31 @@
-%% Vaizdo posukis koordinaciu pradzios atzvilgiu
 clc, clear, close all
 
 I = imread('cameraman.tif');
 Idouble = im2double(I);
 figure, imshow(I)
    
-% posukis pagal laikrodzio rodykle???   
-T1 = [ cos(deg2rad(45)) -sin(deg2rad(45))   0;
-       sin(deg2rad(45))  cos(deg2rad(45))   0;
-            0        0    1 ];
-
+% posukis 45 laipsniu kampu  
+T1 = [ 
+    cos(deg2rad(45)) -sin(deg2rad(45))   0;
+    sin(deg2rad(45))  cos(deg2rad(45))   0;
+         0               0               1; 
+];
 % atspindys   
- T2 = [ -1 0   0;
-        0  -1   0;
-        0  0    1 ];
+ T2 = [ 
+    -1 0   0;
+    0  -1   0;
+    0  0    1 ;
+];
 
 % poslinkis        
-T3 = [ 1  0  0;
-       0  1  0;
-       20  -20.7  1 ];
+T3 = [ 
+   1  0  0;
+   0  1  0;
+   20  -20.7  1;
+];
    
 % matricos nuosekliai sudauginamos
 T_komp = T1*T2*T3;
-
-
 
 T_komp_1 = maketform('affine', T1);
 I1 = imtransform(I, T_komp_1, 'bicubic', 'XYScale', 1);
@@ -40,18 +42,18 @@ figure, imshow(I2); title('atspindys')
 figure, imshow(I3); title('poslinkis')
 figure, imshow(Ilast); title('a')
 
-t1 = maketform('affine',T_komp_.tdata.Tinv);% or t_ = fliptform(T_); 
-I_a = imtransform(I1,t1,'bicubic', 'XYScale', 1);%.........,'FillValues',.3);
+%atstatymas
+t1 = maketform('affine', T_komp_.tdata.Tinv);
+I_a = imtransform(I1, t1, 'bicubic', 'XYScale', 1);
 
+%rezultatai transformacijos
 figure, subplot(1,3,1); imshow(I); title('a) Originalus')
 subplot(1,3,2),imshow(Ilast,'XData',xdata1,'YData',ydata1); title('b) Transformuotas')
 axis on; axis([-300 580 -280 300])
-%axis on; axis([0 660 -160 460])
 subplot(1,3,3), imshow(I_a); title('c) Atstatytas')
 
-I1thresh = I_a >= 100; %// As an example - Note the division by 255 as you did im2double
-%// Removed as this is no longer needed 
-%// sizeI = size(I1);
+%remelio salinimas
+I1thresh = I_a >= 100; 
 nonZeroCols = find(any(I1thresh)); %// Change
 minCol = min(nonZeroCols); %// Change
 maxCol = max(nonZeroCols); %// Change
@@ -62,16 +64,16 @@ minRow = min(nonZeroRows); %// Change
 maxRow = max(nonZeroRows); %// Change
 I_c = I_b(minRow : maxRow, :, :); %// Change
 
+%remelio salinimo vaizdavimas
 figure;
 subplot(1,3,1), imshow(I_a);title('Figure 1');
 subplot(1,3,2), imshow(I_b);title('Figure 2');
 subplot(1,3,3), imshow(I_c);title('Figure 3');
 
+%originaliai gautas paveiksliukas
 figure;
 imshow(I_c);
 
-
-% palyginti gautus paveiksliukus su psnr
-
+% psnr palyginimas
 [ps1, snr1] = psnr(I, I_c);
 fprintf('\n psnr %0.4f \n', snr1);
